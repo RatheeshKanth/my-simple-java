5aceef15cc85521ec4fe0b1cdceee5a71f64c155
RatheeshKanth <riji17@gmail.com>
2022-06-19 19:14:43 +0530

    java_comit

--- my.java
+++ my.java
@@ -1,6 +1,6 @@
 class Simple{
     public static void main(String args[]){
      System.out.println("Hello Riji,");
-     System.out.println("initial commit");
+     System.out.println("java commit");
     }
 }
