cf1d69820118d1a53c80ac6c9116b09c05798e29
RatheeshKanth <riji17@gmail.com>
2022-06-25 18:39:59 +0530

    file_reStructured

--- java/simple.java
+++
@@ -1,8 +0,0 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");
-     System.out.println("This is Mys first");   
-     System.out.println("test success");
-     System.out.println("initial commit");
-    }  
-} 
--- java/test/my.java
+++
@@ -1,7 +0,0 @@
-class Simple{
-    public static void main(String args[]){
-     System.out.println("Hello Riji,");
-     System.out.println("java commit from dev for jenkins");
-     System.out.println("jenkins trigger success");
-    }
-}
